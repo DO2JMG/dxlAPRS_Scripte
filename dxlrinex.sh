984bf4405678daefd7782ab882858471daba8868
Jean-Michael <34538443+DO2JMG@users.noreply.github.com>
2023-10-22 22:44:17 +0200

    Update dxlrinex.sh

--- dxlrinex.sh
+++ dxlrinex.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+
 workingdir="$( cd "$(dirname "$0")" ; pwd -P )"
 
+${workingdir}="$( cd "$(dirname "$0")" ; pwd -P )"
+
 cd /tmp
+
 url=ftp://gssc.esa.int/gnss/data/daily/$(date +%Y)/$(date +%j)/brdc$(date +%j)0.$(date +%y)n.gz
 
 echo $url
 
 if [[ $(wget $url -O-) ]] 2>/dev/null
 then
-  rm ${workingdir/rinex.txt
+  rm ${workingdir}/rinex.txt
   wget $url
   gzip -f -d brdc$(date +%j)'0.'$(date +%y)n.gz
-  mv -f brdc$(date +%j)'0.'$(date +%y)n ${workingdir/rinex.txt
+  mv -f brdc$(date +%j)'0.'$(date +%y)n ${workingdir}/rinex.txt
 fi
